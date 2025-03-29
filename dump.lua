7409e20a8984d95e3e0b6d7cc9cb8fccc0c8b7a9
whosit <knbdevel@gmail.com>
2025-03-29 16:23:16 +0300

    be more consistent and less confusing with dumped object names

--- dump.lua
+++ dump.lua
@@ -56,17 +56,17 @@ local function basic_dump(o)
 	--	return string.format("loadstring(%q)", string.dump(o))
     elseif tp == "userdata" then
         if o.is_valid and not o:is_valid() then
-            return string.format('#<invalid_ref: %s>', o)
+            return string.format('#<Obj:invalid: %s>', o)
         end
         if o.is_player then
             if o:is_player() then
-                return string.format('#<player: "%s">', o:get_player_name())
+                return string.format('#<Obj:player: "%s">', o:get_player_name())
             else
                 local e = o:get_luaentity()
                 if e then
-                    return string.format('#<luaentity: "%s">', e.name)
+                    return string.format('#<Obj:lua: "%s">', e.name)
                 else
-                    return string.format('#<ObjectRef: %s>', o)
+                    return string.format('#<Obj: %s>', o)
                 end
             end
         end
